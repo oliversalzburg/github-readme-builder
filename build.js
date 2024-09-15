b0fa67eb4b8feb99e2cf75dadb302e3b09e71ef4
Oliver Salzburg <oliver.salzburg@protonmail.com>
2024-09-15 14:03:11 +0200

    Initial commit

---
+++ build.js
@@ -0,0 +1,14 @@
+import { redirectErrorsToConsole } from "@oliversalzburg/js-utils/errors/console.js";
+import esbuild from "esbuild";
+
+esbuild
+  .build({
+    bundle: true,
+    entryPoints: ["./source/main.ts"],
+    external: ["os"],
+    format: "esm",
+    outfile: "./output/main.js",
+    platform: "node",
+    target: "node20",
+  })
+  .catch(redirectErrorsToConsole(console));
